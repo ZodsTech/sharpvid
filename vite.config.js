b89c5320279be5abd2ad5360510a197dbeae6806
ZodsTech <chukwunonsoozodi@gmail.com>
2026-03-04 15:43:04 +0000

    Fix project structure for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "/sharpvid",
   server: {
-    host: "0.0.0.0",
-
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false
-      }
-    },
-
-    allowedHosts: [
-      "f3b91ffe-1f52-4689-863b-502036474519-00-qwxb21kkmao6.worf.replit.dev"
-    ]
-  }
-});
+      host: "0.0.0.0",
+          port: 5173,
+              strictPort: true,
+                  proxy: {
+                        "/api": {
+                                target: "http://localhost:3000",
+                                        changeOrigin: true,
+                                                secure: false
+                                                      }
+                                                          }
+                                                            }
+                                                            });
